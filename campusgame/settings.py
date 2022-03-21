e1ebe8366b464bbe07073f977b5bfee8fba58c09
the_mason45 <sam@themasons.org>
2022-03-21 21:01:33 +0000

    Updated CORS allowed

--- campusgame/settings.py
+++ campusgame/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 import dj_database_url
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tbqhbgfw4!!8w%$pf@onfnz@(wg7j4k%ngay740r)mp0u!f-yu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = ['*']
 
@@ -64,6 +65,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://campusgame.themasons.org",
 ]
 
 ROOT_URLCONF = 'campusgame.urls'
