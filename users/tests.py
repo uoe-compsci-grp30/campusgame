fe859a42f4445c13457c53d0e16f1e4708f36a4c
jron-reb <jeroenmijer@tutanota.com>
2022-03-23 21:44:47 +0000

    beginneing of tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,73 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from django.urls import reverse
+from django.db import models
+import uuid
 
-# Create your tests here.
+from.models import User
+
+class HomepageTests(TestCase):
+    """
+    Class responsible for testing the homepage
+    """
+
+    #def setUp(self):
+    #    User.objects.create(id=1)
+
+    def test_admin_login(self):
+        #uuid_temp = models.UUIDField(default=uuid.uuid4, primary_key=True)
+        my_admin = User(username='user')
+        my_admin.set_password('password')
+        my_admin.is_superuser = True
+        my_admin.is_staff = True
+        my_admin.save() #saves the user in the database
+        response_login_page = self.client.get('/admin/login/?next=/admin')
+        self.assertEquals(response_login_page.status_code, 200) #this tests to see that the user is on the login page
+        
+        loginresponse = self.client.login(username='user', password='passwrd')
+        self.assertTrue(loginresponse) # that there was a response in regards to an attempt to login, does not show a successful login or not
+
+        #normally the test would work as above with testing if the status code is 200. However due to issues with the migrations file when typing in the url '/admin/' (note the slash at the end) rather than '/admin' it throws an exception. To combat this testing is done for the url '/admin' (this could be any url) and ensure that the response code is 301 and the new url is '/admin/' (the changed url is provided in the response)
+        response_logged_in = self.client.get('/admin/login/?next=/admin')
+        #self.assertFalse(response_logged_in.status_code, 200)
+        #self.assertEquals(response_logged_in.url, '/admin/')
+
+        
+    """ def test_admin_login2(self):
+
+        class MockUser:
+            is_authenticated = True
+
+        request = self.factory.get('/admin/login/?next=/admin')
+        request.user = MockUser()
+        response = news(request)
+        self.assertContains(response, 'Logout') """ 
+
+
+    def test_admin_page_status_code(self):
+        response = self.client.get('/admin/login/?next=/admin') #isn't just /admin because during the process the url changes from admin into the url shown. If just /admin was tested it throws HTTP code 301 (URL moved permanently)
+        self.assertEquals(response.status_code, 200)
+
+    """ def test_admin_page_status_code_logged_in(self):
+        class MockUser:
+            is_authenticated = True
+
+        user = MockUser()
+        response = user.client.get('/admin.')
+        self.assertAlmostEquals(response.status_code,200)""" 
+        #self notes
+        #essentially right here tryna create a mock object that is authenticated to see if the login works
+
+
+    #def test_user_page_status_code(self):
+    #    response = self.client.get('/users')
+    #    self.assertEquals(response.status_code, 200)
+
+    #def test_user_page_status_code(self):
+    #def test_games_page_status_code(self):
+    #    response = self.client.get('/api/games')
+    #    self.assertEquals(response.status_code, 200)
+    #def test_view_url_by_name(self):
+    #def test_games_page_status_code(self):
+    #    response = self.client.get(reverse('/admin'))
+    #    self.assertEquals(response.status_code, 200)
